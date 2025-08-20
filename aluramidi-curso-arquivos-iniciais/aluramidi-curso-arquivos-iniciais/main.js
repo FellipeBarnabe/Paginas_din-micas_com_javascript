7382f7a9ea9311ac0b3694329717c5faf290b19f
Fellipe Barnabe <fellipebarnabe.stack@gmail.com>
2025-08-20 18:15:01 -0300

    construindo funcao

--- aluramidi-curso-arquivos-iniciais/aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,34 @@
-function TocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaMidi(idElementAudio){
+    document.querySelector(idElementAudio).play()
+
 }
 
+
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contator = 0;
+
+while(contador < listaDeTeclas.length){
+listaDeTeclas[contator].onclick = tocaMidi;
+    contator = contador +1;
+listaDeTeclas[1].onclick =  idElementAudio;  
+
+}
+
+
+/* listaDeTeclas.forEach(tecla => {
+    tecla.addEventListener('click', () => {
+      // Pega a segunda classe (ex: tecla_pom)
+      const instrumento = tecla.classList[1];
+      // Monta o id do áudio (ex: som_tecla_pom)
+      const idAudio = `#som_${instrumento}`;
+      const audio = document.querySelector(idAudio);
+
+      if (audio) {
+        audio.currentTime = 0; // volta o áudio pro início
+        audio.play();          // toca o som
+      }
+    });
+  }); */
+  
+
